d911e89f3b3d81547b000b03963d4419fe4eee35
Brandon Murry <BrandonFOSSEdu@gmail.com>
2023-01-31 11:10:28 -0500

    3rd period examples

--- formatted-input.c
+++ formatted-input.c
@@ -5,10 +5,26 @@ int main() {
     char str[20];
 
     printf("Enter your age followed by your name: ");
-    scanf("%i %s", &a, str);
+    scanf("%i,%s", &a, str);
 
     printf("Your age is: %i\n", a);
+    printf("The address of a is %p\n", &a);
     printf("Your name is: %s\n", str);
+    printf("The address of str is %p\n", str);
+
+    scanf("%i,%s", &a, str);
+
+    printf("Your age is: %i\n", a);
+    printf("The address of a is %p\n", &a);
+    printf("Your name is: %s\n", str);
+    printf("The address of str is %p\n", str);
+
+    scanf("%i,%s", &a, str);
+
+    printf("Your age is: %i\n", a);
+    printf("The address of a is %p\n", &a);
+    printf("Your name is: %s\n", str);
+    printf("The address of str is %p\n", str);
 
     return 0;
 }
---
+++ formatted-strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Prompt user for two strings, separated by a space
+
+int main() {
+    char s1[20], s2[20];
+
+    // Give user directions
+    printf("");
+    // Get user input into 2 strings
+    scanf("", s1, s2);
+    // Print out strings in reverse order, using width specifier
+    printf("");
+    return 0;
+
+}
